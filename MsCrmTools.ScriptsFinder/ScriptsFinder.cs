7d85dc92cbcf8b5058df3591571cfbd536f510a7
Tanguy TOUZARD <tanguy92@hotmail.com>
2016-11-16 17:35:00 +0100

    Initial release

---
+++ MsCrmTools.ScriptsFinder/ScriptsFinder.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+
+namespace MsCrmTools.ScriptsFinder
+{
+    public partial class ScriptsFinder : PluginControlBase
+    {
+        #region Constructor
+
+        public ScriptsFinder()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructor
+
+        public void FindScripts()
+        {
+            lvScripts.Items.Clear();
+            tsbMainFindScripts.Enabled = false;
+            tsbExportToCsv.Enabled = false;
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Loading scripts (this can take a while...)",
+                AsyncArgument = null,
+                Work = (bw, e) =>
+                {
+                    var lScripts = new List<ListViewItem>();
+
+                    var sManager = new ScriptsManager(Service, bw);
+                    sManager.Find(new Version(ConnectionDetail.OrganizationVersion));
+
+                    foreach (var script in sManager.Scripts)
+                    {
+                        
+                        var item = new ListViewItem(script.Type) {Tag = script};
+                        item.SubItems.Add(script.EntityName);
+                        item.SubItems.Add(script.EntityLogicalName);
+                        item.SubItems.Add(script.Name);
+                        item.SubItems.Add(script.Event);
+                        item.SubItems.Add(script.Attribute);
+                        item.SubItems.Add(script.AttributeLogicalName);
+                        item.SubItems.Add(script.ScriptLocation);
+                        item.SubItems.Add(script.MethodCalled);
+                        item.SubItems.Add(script.PassExecutionContext?.ToString() ?? "");
+                        item.SubItems.Add(script.Arguments);
+                        item.SubItems.Add(script.IsActive?.ToString() ?? "");
+
+                        if (script.HasProblem)
+                        {
+                            item.ForeColor = Color.Red;
+                        }
+                        lScripts.Add(item);
+                    }
+
+                    e.Result = lScripts;
+                },
+                PostWorkCallBack = e =>
+                {
+                    lvScripts.Items.AddRange(((List<ListViewItem>)e.Result).ToArray());
+                    tsbMainFindScripts.Enabled = true;
+                    tsbExportToCsv.Enabled = true;
+                },
+                ProgressChanged = e =>
+                {
+                    SetWorkingMessage(e.UserState.ToString());
+                }
+            });
+        }
+
+        private void LvScriptsColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            lvScripts.Sorting = lvScripts.Sorting == SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending;
+            lvScripts.ListViewItemSorter = new ListViewItemComparer(e.Column, lvScripts.Sorting);
+        }
+
+        private void TsbCloseThisTabClick(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void TsbExportToCsvClick(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog
+            {
+                Filter = "CSV file (*.csv)|*.csv",
+                Title = "Select a file where to save the list of scripts"
+            };
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (var fs = new FileStream(sfd.FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    var preamble = new UTF8Encoding(true).GetPreamble();
+                    fs.Write(preamble, 0, preamble.Length);
+
+                    var header = Encoding.UTF8.GetBytes(
+                        "Type,Entity Display Name,Entity Logical Name,Form name,Event,Control,Control Logical Name,Script Location,Method Called,Parameters,Enabled,Pass Execution context" +
+                        Environment.NewLine);
+                    fs.Write(header, 0, header.Length);
+
+                    foreach (ListViewItem item in lvScripts.Items)
+                    {
+                        var script = (Script)item.Tag;
+
+                        var line = Encoding.UTF8.GetBytes(string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}{12}",
+                            script.Type,
+                            script.EntityName,
+                            script.EntityLogicalName,
+                            script.Name,
+                            script.Event,
+                            script.Attribute,
+                            script.AttributeLogicalName,
+                            script.ScriptLocation,
+                            script.MethodCalled,
+                            script.Arguments?.Replace(","," "),
+                            script.IsActive,
+                            script.PassExecutionContext,
+                            Environment.NewLine));
+
+                        fs.Write(line, 0, line.Length);
+                    }
+
+                    if (MessageBox.Show(this, string.Format("File saved to '{0}'!\r\n\r\nWould you like to open the file now?", sfd.FileName), "Question",
+                                    MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                    {
+                        Process.Start(sfd.FileName);
+                    }
+                }
+            }
+        }
+
+        private void TsbMainFindScriptsClick(object sender, EventArgs e)
+        {
+            ExecuteMethod(FindScripts);
+        }
+    }
+}
---
+++ MsCrmTools.ScriptsFinder/ScriptsManager.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata.Query;
+
+namespace MsCrmTools.ScriptsFinder
+{
+    public class ScriptsManager
+    {
+        private readonly IOrganizationService service;
+        private readonly BackgroundWorker worker;
+        private EntityMetadataCollection emds;
+        private int userLcid;
+
+        public ScriptsManager(IOrganizationService service, BackgroundWorker worker)
+        {
+            this.service = service;
+            this.worker = worker;
+            Scripts = new List<Script>();
+        }
+
+        public List<Script> Scripts { get; private set; }
+
+        public void Find(Version crmVersion)
+        {
+            worker.ReportProgress(0, "Loading User language...");
+            GetCurrentUserLcid();
+
+            worker.ReportProgress(0, "Loading Entities metadata...");
+            emds = GetEntities();
+
+            worker.ReportProgress(0, "Loading Scripts...");
+            foreach (var emd in emds.Where(x => x.DisplayName.UserLocalizedLabel != null))
+            {
+                LoadScripts(emd);
+                LoadRibbonCommands(emd);
+
+                // Grid events are only available since CRM 8.2
+                if (crmVersion >= new Version(8,2,0,0))
+                {
+                    LoadCustomControls(emd);
+                }
+            }
+        }
+
+        private EntityMetadataCollection GetEntities()
+        {
+            EntityQueryExpression entityQueryExpression = new EntityQueryExpression()
+            {
+                Criteria = new MetadataFilterExpression(LogicalOperator.Or)
+                {
+                    Conditions =
+                    {
+                        new MetadataConditionExpression("IsCustomizable", MetadataConditionOperator.Equals, true),
+                        new MetadataConditionExpression("IsManaged", MetadataConditionOperator.Equals, false),
+                    }
+                },
+                Properties = new MetadataPropertiesExpression
+                {
+                    AllProperties = false,
+                    PropertyNames = { "DisplayName", "LogicalName", "Attributes", "ObjectTypeCode" }
+                },
+                AttributeQuery = new AttributeQueryExpression
+                {
+                    // Récupération de l'attribut spécifié
+                    Properties = new MetadataPropertiesExpression
+                    {
+                        AllProperties = false,
+                        PropertyNames = { "DisplayName", "LogicalName" }
+                    }
+                },
+            };
+
+            RetrieveMetadataChangesRequest retrieveMetadataChangesRequest = new RetrieveMetadataChangesRequest
+            {
+                Query = entityQueryExpression,
+                ClientVersionStamp = null
+            };
+
+            var response = (RetrieveMetadataChangesResponse)service.Execute(retrieveMetadataChangesRequest);
+
+            return response.EntityMetadata;
+        }
+
+        private void GetCurrentUserLcid()
+        {
+            var user = (WhoAmIResponse)service.Execute(new WhoAmIRequest());
+
+            var userSettings = service.RetrieveMultiple(new QueryByAttribute("usersettings")
+            {
+                Attributes = { "systemuserid" },
+                Values = { user.UserId },
+            }).Entities.First();
+
+            userLcid = userSettings.GetAttributeValue<int>("uilanguageid");
+        }
+
+        private void LoadRibbonCommands(EntityMetadata emd)
+        {
+            var commands = service.RetrieveMultiple(new QueryExpression("ribboncommand")
+            {
+                ColumnSet = new ColumnSet(true),
+                Criteria = new FilterExpression
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression("commanddefinition", ConditionOperator.Like, "%Library=\"$webresource:%"),
+                        new ConditionExpression("entity", ConditionOperator.Equal, emd.LogicalName)
+                    }
+                }
+            });
+
+            foreach (var command in commands.Entities)
+            {
+                var commandDoc = new XmlDocument();
+                commandDoc.LoadXml(command.GetAttributeValue<string>("commanddefinition"));
+
+                var actionsNode = commandDoc.SelectSingleNode("CommandDefinition/Actions");
+                if (actionsNode == null)
+                {
+                    return;
+                }
+
+                foreach (XmlNode actionNode in actionsNode.ChildNodes)
+                {
+                    var libraryNode = actionNode.Attributes?["Library"];
+                    if (libraryNode == null)
+                    {
+                        continue;
+                    }
+
+                    var libraryName = libraryNode.Value;
+
+                    if (libraryName.Split(':').Length == 1)
+                        continue;
+
+                    var script = new Script();
+                    script.EntityLogicalName = emd.LogicalName;
+                    script.EntityName = emd.DisplayName.UserLocalizedLabel.Label;
+                    script.ScriptLocation = libraryName.Split(':')[1];
+                    script.MethodCalled = actionNode.Attributes["FunctionName"].Value;
+                    script.Event = "";
+                    script.Attribute = "";
+                    script.AttributeLogicalName = "";
+                    script.Name = string.Empty;
+                    script.Type = "Ribbon Command";
+
+                    var parameters = new List<string>();
+
+                    foreach (XmlNode parameterNode in actionNode.ChildNodes)
+                    {
+                        // ReSharper disable once PossibleNullReferenceException
+                       parameters.Add(string.Format("{0}:{1}", parameterNode.Name, parameterNode.Attributes["Value"].Value));
+                    }
+
+                    script.Arguments = string.Join(" / ", parameters);
+
+                    Scripts.Add(script);
+                }
+            }
+        }
+
+        private void LoadScripts(EntityMetadata emd)
+        {
+            var qba = new QueryByAttribute("systemform");
+            qba.Attributes.Add("objecttypecode");
+            // ReSharper disable once PossibleInvalidOperationException
+            qba.Values.Add(emd.ObjectTypeCode.Value);
+            qba.ColumnSet = new ColumnSet(true);
+
+            foreach (var form in service.RetrieveMultiple(qba).Entities)
+            {
+                var doc = new XmlDocument();
+                doc.LoadXml(form["formxml"].ToString());
+
+                foreach (XmlNode eventNode in doc.SelectNodes("//event"))
+                {
+                    string eventName = eventNode.Attributes["name"].Value;
+
+                    foreach (XmlNode handlerNode in eventNode.SelectNodes("Handlers/Handler"))
+                    {
+                        var script = new Script();
+                        script.EntityLogicalName = emd.LogicalName;
+                        script.EntityName = emd.DisplayName.UserLocalizedLabel.Label;
+                        script.ScriptLocation = handlerNode.Attributes["libraryName"].Value;
+                        script.MethodCalled = handlerNode.Attributes["functionName"].Value;
+                        script.IsActive = handlerNode.Attributes["enabled"].Value == "true";
+                        script.PassExecutionContext = handlerNode.Attributes["passExecutionContext"]?.Value == "true";
+                        script.Event = eventName;
+                        script.Arguments = handlerNode.Attributes["parameters"] != null ? handlerNode.Attributes["parameters"].Value : "";
+                        script.Type = "Form event";
+
+                        if (eventName == "onchange")
+                        {
+                            var amd = emd.Attributes.FirstOrDefault(x => eventNode.Attributes != null && x.LogicalName == eventNode.Attributes["attribute"].Value);
+
+                            if (amd != null)
+                            {
+                                var displayName = amd.DisplayName != null && amd.DisplayName.UserLocalizedLabel != null
+                                    ? amd.DisplayName.UserLocalizedLabel.Label
+                                    : "(" + amd.LogicalName + ")";
+
+                                script.Attribute = displayName;
+                                script.AttributeLogicalName = amd.LogicalName;
+                            }
+                            else if (eventNode.Attributes["control"] == null)
+                            {
+                                script.Attribute = eventNode.Attributes["attribute"].Value;
+                                script.AttributeLogicalName = eventNode.Attributes["attribute"].Value;
+                                script.HasProblem = true;
+                            }
+                            else
+                            {
+                                XmlNode node = doc.SelectSingleNode("//control[@id='" + eventNode.Attributes["control"].Value + "']");
+                                XmlNode targetEntityNode = node.SelectSingleNode("parameters/TargetEntityType");
+
+                                amd = emds.First(e=>e.LogicalName == targetEntityNode.InnerText).Attributes.First(x => x.LogicalName == eventNode.Attributes["attribute"].Value);
+                                var displayName = amd.DisplayName != null && amd.DisplayName.UserLocalizedLabel != null
+                                   ? amd.DisplayName.UserLocalizedLabel.Label
+                                   : "(" + amd.LogicalName + ")";
+
+                                script.Attribute = displayName;
+                                script.AttributeLogicalName = amd.LogicalName;
+                            }
+
+                            if (eventNode.Attributes["control"] != null)
+                            {
+                                var control = eventNode.Attributes["control"].Value;
+
+                                XmlNode node = doc.SelectSingleNode("//control[@id='" + control + "']");
+
+                                var labelNode = node.ParentNode.SelectSingleNode("labels/label[@languagecode='" + userLcid + "']");
+                                if (labelNode == null)
+                                {
+                                    labelNode = node.ParentNode.SelectSingleNode("labels/label");
+                                }
+
+                                var label = labelNode.Attributes["description"].Value;
+
+                                script.Type = "Subgrid event";
+                                script.Attribute = string.Format("{0} / {1}", label, script.Attribute);
+                                script.AttributeLogicalName = string.Format("{0} / {1}", control, script.AttributeLogicalName);
+                            }
+                        }
+                        else if (eventName == "onrecordselect")
+                        {
+                            var control = eventNode.Attributes["control"].Value;
+
+                            XmlNode node = doc.SelectSingleNode("//control[@id='" + control + "']");
+                            var label = node.ParentNode.SelectSingleNode("labels/label[@languagecode='1036']").Attributes[
+                                    "description"].Value;
+
+                            script.Type = "Subgrid event";
+                            script.Attribute = label;
+                            script.AttributeLogicalName = control;
+                        }
+                        else if (eventName == "onsave")
+                        {
+                            if (eventNode.Attributes["control"] != null)
+                            {
+                                var control = eventNode.Attributes["control"].Value;
+
+                                XmlNode node = doc.SelectSingleNode("//control[@id='" + control + "']");
+                                var label = node.ParentNode.SelectSingleNode("labels/label[@languagecode='1036']").Attributes[
+                                        "description"].Value;
+
+                                script.Type = "Subgrid event";
+                                script.Attribute = label;
+                                script.AttributeLogicalName = control;
+                            }
+                        }
+                        else
+                        {
+                            script.Attribute = "";
+                            script.AttributeLogicalName = "";
+                        }
+                        script.Name = form["name"].ToString();
+
+                        Scripts.Add(script);
+                    }
+                }
+
+                foreach (XmlNode libraryNode in doc.SelectNodes("//Library"))
+                {
+                    var script = new Script();
+                    script.EntityLogicalName = emd.LogicalName;
+                    script.EntityName = emd.DisplayName.UserLocalizedLabel.Label;
+                    script.ScriptLocation = libraryNode.Attributes["name"].Value;
+                    script.MethodCalled = string.Empty;
+                    script.Event = string.Empty;
+                    script.Attribute = string.Empty;
+                    script.AttributeLogicalName = string.Empty;
+                    script.Name = form["name"].ToString();
+                    script.Type = "Form Library";
+
+                    Scripts.Add(script);
+                }
+            }
+        }
+
+        private void LoadCustomControls(EntityMetadata emd)
+        {
+            var ccs = service.RetrieveMultiple(new QueryExpression("customcontroldefaultconfig")
+            {
+                ColumnSet = new ColumnSet("eventsxml"),
+                Criteria = new FilterExpression
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression("primaryentitytypecode", ConditionOperator.Equal, emd.ObjectTypeCode.Value),
+                        new ConditionExpression("eventsxml", ConditionOperator.NotNull)
+                    }
+                }
+            });
+
+            foreach (var cc in ccs.Entities)
+            {
+                var doc = new XmlDocument();
+                doc.LoadXml(cc.GetAttributeValue<string>("eventsxml"));
+
+                foreach (XmlNode eventNode in doc.SelectNodes("//event"))
+                {
+                    string eventName = eventNode.Attributes["name"].Value;
+
+                    foreach (XmlNode handlerNode in eventNode.SelectNodes("Handlers/Handler"))
+                    {
+                        var script = new Script();
+                        script.EntityLogicalName = emd.LogicalName;
+                        script.EntityName = emd.DisplayName.UserLocalizedLabel.Label;
+                        script.ScriptLocation = handlerNode.Attributes["libraryName"].Value;
+                        script.MethodCalled = handlerNode.Attributes["functionName"].Value;
+                        script.IsActive = handlerNode.Attributes["enabled"].Value == "true";
+                        script.PassExecutionContext = handlerNode.Attributes["passExecutionContext"]?.Value == "true";
+                        script.Event = eventName;
+                        script.Arguments = handlerNode.Attributes["parameters"] != null ? handlerNode.Attributes["parameters"].Value : "";
+                        script.Type = "Homepage Grid event";
+
+                        if (eventName == "onchange")
+                        {
+                            var amd = emd.Attributes.FirstOrDefault(x => x.LogicalName == eventNode.Attributes["attribute"].Value);
+
+                            if (amd != null)
+                            {
+                                var displayName = amd.DisplayName != null && amd.DisplayName.UserLocalizedLabel != null
+                                    ? amd.DisplayName.UserLocalizedLabel.Label
+                                    : "(" + amd.LogicalName + ")";
+
+                                script.Attribute = displayName;
+                                script.AttributeLogicalName = amd.LogicalName;
+                            }
+                        }
+                        else
+                        {
+                            script.Attribute = "";
+                            script.AttributeLogicalName = "";
+                        }
+
+                        Scripts.Add(script);
+                    }
+                }
+
+                foreach (XmlNode libraryNode in doc.SelectNodes("//Library"))
+                {
+                    var script = new Script();
+                    script.EntityLogicalName = emd.LogicalName;
+                    script.EntityName = emd.DisplayName.UserLocalizedLabel.Label;
+                    script.ScriptLocation = libraryNode.Attributes["name"].Value;
+                    script.MethodCalled = string.Empty;
+                    script.Event = string.Empty;
+                    script.Attribute = string.Empty;
+                    script.AttributeLogicalName = string.Empty;
+                    script.Type = "Homepage Grid Library";
+
+                    Scripts.Add(script);
+                }
+            }
+        }
+    }
+}
